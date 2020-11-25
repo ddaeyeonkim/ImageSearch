7d8e2b5464bc9974309f8649d6f0daf2c2f6a866
improve777 <improve777@gmail.com>
2020-11-26 06:16:51 +0900

    Timber 초기화 코드 추가

--- app/src/main/java/com/improve777/imagesearch/App.kt
+++ app/src/main/java/com/improve777/imagesearch/App.kt
@@ -5,6 +5,8 @@ import com.improve777.imagesearch.di.AppComponent
 import com.improve777.imagesearch.di.DaggerAppComponent
 import com.improve777.imagesearch.di.DaggerComponentProvider
 import com.jakewharton.threetenabp.AndroidThreeTen
+import timber.log.Timber
+import timber.log.Timber.DebugTree
 
 class App : Application(), DaggerComponentProvider {
 
@@ -13,6 +15,7 @@ class App : Application(), DaggerComponentProvider {
     override fun onCreate() {
         super.onCreate()
         AndroidThreeTen.init(this)
+        initTimber()
     }
 
     private fun initAppComponent(): AppComponent {
@@ -22,5 +25,11 @@ class App : Application(), DaggerComponentProvider {
                 BuildConfig.DEBUG,
                 BuildConfig.KAKAO_API_KEY,
             )
+    }
+
+    private fun initTimber() {
+        if (BuildConfig.DEBUG) {
+            Timber.plant(DebugTree())
+        }
     }
 }
