af2cad54f759a532de4f1f9aea21f55b525a5f0a
improve777 <improve777@gmail.com>
2020-11-26 02:07:05 +0900

    NetworkModule 추가

---
+++ app/src/main/java/com/improve777/imagesearch/di/NetworkModule.kt
@@ -0,0 +1,52 @@
+package com.improve777.imagesearch.di
+
+import com.improve777.imagesearch.data.remote.api.ImageApi
+import dagger.Module
+import dagger.Provides
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
+import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
+import javax.inject.Singleton
+
+@Module
+object NetworkModule {
+
+    @Singleton
+    @Provides
+    fun provideImageApi(retrofit: Retrofit): ImageApi {
+        return retrofit.create(ImageApi::class.java)
+    }
+
+    @Singleton
+    @Provides
+    fun provideRetrofit(baseUrl: String, client: OkHttpClient): Retrofit {
+        return Retrofit.Builder()
+            .baseUrl(baseUrl)
+            .client(client)
+            .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+    }
+
+    @Singleton
+    @Provides
+    fun provideOkHttpClient(loggingInterceptor: HttpLoggingInterceptor): OkHttpClient {
+        return OkHttpClient.Builder()
+            .connectTimeout(15, TimeUnit.SECONDS)
+            .readTimeout(15, TimeUnit.SECONDS)
+            .writeTimeout(15, TimeUnit.SECONDS)
+            .addInterceptor(loggingInterceptor)
+            .build()
+    }
+
+    @Singleton
+    @Provides
+    fun provideLoggingInterceptor(isDebug: Boolean): HttpLoggingInterceptor {
+        return HttpLoggingInterceptor().apply {
+            level = if (isDebug) HttpLoggingInterceptor.Level.BODY else HttpLoggingInterceptor.Level.NONE
+        }
+    }
+}
